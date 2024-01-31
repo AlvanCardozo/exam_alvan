37a9c6b93b94c568e7c4dc7f8acc652942e59e26
Alvan Cardozo <rbca22107.rcca@gmail.com>
2024-01-31 10:33:27 +0530

    commit

---
+++ src/exam107/vehicle_107.java
@@ -0,0 +1,16 @@
+package exam107;
+
+public class vehicle_107 {
+	String brand;
+	int speed;
+	
+	vehicle_107(String brand,int speed){
+		this.brand=brand;
+		this.speed=0;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
